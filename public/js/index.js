e58a2727a6e3d6e6b08387751a4afafb7a2e895e
jarod <firius87@gmail.com>
2017-09-16 20:55:43 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Yup, that works me.'
-    });
 });
 
 socket.on('disconnect', function() {
@@ -14,8 +10,4 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
-    // socket.emit('createMessage', {
-    //     from: 'Andrew',
-    //     text: 'Yup, that works me.'
-    // });
 });
