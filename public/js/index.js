6dd105d872bcccea4c79db87059814b22341cbf1
jarod <firius87@gmail.com>
2017-09-17 00:45:59 +0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -17,11 +17,12 @@ socket.on('newMessage', function(message) {
 
 $('#message-form').on('submit', function(e) {
     e.preventDefault();
+    var messageTextbox = $('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-        // console.log('Got it', data);
+        messageTextbox.val('');
     });
 });
 
@@ -35,17 +36,22 @@ socket.on('newLocationMessage', function(message) {
     $('#messages').append(li);
 });
 
-$('#send-location').on('click', function() {
+var locationButton = $('#send-location');
+locationButton.on('click', function() {
     if(!navigator.geolocation) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fecth location.');
     });
 });
