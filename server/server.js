05c18a33bb2b272226b89726e4a4dfbb5eeaf108
jarod <firius87@gmail.com>
2017-09-17 21:33:17 +0200

    Add README.md

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ io.on('connection', (socket) => {
 
         io.to(params.room).emit('updateUserList', users.getUserList(params.room));
         socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joind.`));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
 
         callback();
     });
